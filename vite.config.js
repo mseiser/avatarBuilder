4031e977dca0f616bc102979a206ab1a482518f0
Maria Seiser <seiser@student.tugraz.at>
2025-03-11 10:42:40 +0100

    adapt deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/avatarBuilder/',
+  base: '/avatarBuilder/s',
   plugins: [react(),
     tailwindcss(),
   ],
